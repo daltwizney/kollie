c783fb3f058c670d448aed151eff595091a8f6df
danrae pray <danrae.pray@outlook.com>
2025-01-11 10:46:58 -0600

    added canvas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.wizneylabs.kollie"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.wizneylabs.kollie"
--- app/src/main/java/com/wizneylabs/kollie/MainActivity.kt
+++ app/src/main/java/com/wizneylabs/kollie/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wizneylabs.kollie.ui.theme.KollieTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KollieTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyCanvas() {
+    Canvas(modifier = Modifier
+        .padding(vertical = 20.dp)
+        .fillMaxSize()) {
+
+//        drawRect(color = Color.Blue);
+
+        val multiColorGradient = Brush.verticalGradient(
+            0f to Color.Red,
+            0.3f to Color.Yellow,
+            0.6f to Color.Green,
+            1f to Color.Blue
+        )
+
+        drawRect(brush = multiColorGradient);
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun CanvasPreview() {
     KollieTheme {
-        Greeting("Android")
+        MyCanvas()
     }
 }
