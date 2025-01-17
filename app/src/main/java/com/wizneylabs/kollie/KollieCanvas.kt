1a1cf1edcc254355a9d17c03b1b2be17dd999750
danrae pray <danrae.pray@outlook.com>
2025-01-17 17:27:43 -0600

    got realtime loop working

--- app/src/main/java/com/wizneylabs/kollie/KollieCanvas.kt
+++ app/src/main/java/com/wizneylabs/kollie/KollieCanvas.kt
@@ -1,5 +1,6 @@
 package com.wizneylabs.kollie
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,34 +10,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-data class GameState(
-    val deltaTime: Float = 0f,
-    val lastFrameTimeMillis: Long = System.currentTimeMillis()
-)
-
 @Composable
 fun KollieCanvas(viewModel: MainViewModel) {
-
-    var time by remember { mutableStateOf(0f) }
-
-    LaunchedEffect(Unit) {
-        while (true) {
-            withFrameNanos { frameTimeNanos ->
-                time = (frameTimeNanos / 1_000_000_000f)
-            }
-        }
-    }
 
     Canvas(modifier = Modifier
         .padding(vertical = 20.dp)
         .fillMaxSize()
     ) {
+        Log.d("MainViewModel", "re-rendering!");
+
+        @Suppress("UNUSED_VARIABLE")
+        val invalidator = viewModel.frameCounter.value;
 
         drawCircle(color = Color.Blue,
             center = Offset(150f, 150f),
--- app/src/main/java/com/wizneylabs/kollie/MainViewModel.kt
+++ app/src/main/java/com/wizneylabs/kollie/MainViewModel.kt
@@ -1,15 +1,51 @@
 package com.wizneylabs.kollie
 
+import android.util.Log
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
+
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainViewModel : ViewModel() {
+
+    public var frameCounter = mutableStateOf(0);
 
     private val _count = mutableStateOf(0);
 
     public val Count = derivedStateOf {
         "Count: ${_count.value}"
+    }
+
+    init {
+
+        _startSimulation();
+    }
+
+    private var simJob: Job? = null;
+
+    private fun _startSimulation() {
+
+        Log.d("MainViewModel", "started simulation!");
+
+        simJob?.cancel();
+
+        simJob = viewModelScope.launch {
+
+            Log.d("MainViewModel", "launching coroutines!");
+
+            while (true) {
+
+                delay(100);
+
+                frameCounter.value++;
+            }
+        }
     }
 
     fun increment() {
