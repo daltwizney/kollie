a064a322d52d4937578de96283259d4e86e7c02d
danrae pray <danrae.pray@outlook.com>
2025-02-12 20:10:39 -0600

    custom visual transformation is crashing app when textfield clicked twice for some reason... removed it from text field for now. + added some navigation.

--- freestyle/src/main/java/com/wizneylabs/freestyle/FreestyleViewModel.kt
+++ freestyle/src/main/java/com/wizneylabs/freestyle/FreestyleViewModel.kt
@@ -1,10 +1,15 @@
 package com.wizneylabs.freestyle
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.withStyle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,6 +17,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class FreestyleViewModel: ViewModel() {
+
+    val TAG = FreestyleViewModel::class.simpleName;
+
+    init {
+
+        Log.d(TAG, "viewmodel created!");
+    }
 
     private val _editorText = MutableStateFlow("");
 
--- freestyle/src/main/java/com/wizneylabs/freestyle/MainActivity.kt
+++ freestyle/src/main/java/com/wizneylabs/freestyle/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
@@ -19,7 +27,12 @@ import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.wizneylabs.freestyle.ui.theme.KollieTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,12 +44,65 @@ class MainActivity : ComponentActivity() {
 
                 val viewModel: FreestyleViewModel = viewModel();
 
-                FreestyleEditor(viewModel);
+                FreeStyleApp(viewModel);
             }
         }
     }
 }
 
+@Composable
+fun FreeStyleApp(viewModel: FreestyleViewModel) {
+
+    val navController = rememberNavController();
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            MainMenu(navController, viewModel);
+        }
+        composable("editor") {
+            FreestyleEditor(navController, viewModel);
+        }
+    }
+}
+
+@Composable
+fun MainMenu(navController: NavHostController, viewModel: FreestyleViewModel) {
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+
+        Text("Item 1")
+        Text("Item 2")
+        Text("Item 3")
+
+        Button(onClick = { navController.navigate("editor") }) {
+            Text("Editor");
+        }
+    }
+}
+
+@Composable
+fun FreestyleEditor(navController: NavHostController, viewModel: FreestyleViewModel) {
+
+    val editorText = viewModel.editorText.collectAsState();
+
+    TextField(
+        value = editorText.value,
+        onValueChange = { viewModel.onEditorTextChanged(it) },
+        modifier = Modifier
+            .fillMaxSize(),
+        textStyle = MaterialTheme.typography.bodyLarge,
+        colors = TextFieldDefaults.colors()
+    )
+}
+
+// TODO: using this with a TextField composable causes crash...
 class LanguageKeywordColorTransformation() : VisualTransformation {
 
     override fun filter(text: AnnotatedString): TransformedText {
@@ -59,24 +125,10 @@ class LanguageKeywordColorTransformation() : VisualTransformation {
                 appendLine("----")
                 withStyle(SpanStyle(color = Color.Blue)) {
                     appendLine("it's about to go down");
-                }            },
+                }
+            },
 
-            OffsetMapping.Identity);
+            OffsetMapping.Identity
+        );
     }
-}
-
-@Composable
-fun FreestyleEditor(viewModel: FreestyleViewModel) {
-
-    val editorText = viewModel.editorText.collectAsState();
-
-    TextField(
-        value = editorText.value,
-        onValueChange = { viewModel.onEditorTextChanged(it) },
-        modifier = Modifier
-            .fillMaxSize(),
-        textStyle = MaterialTheme.typography.bodyLarge,
-        visualTransformation = LanguageKeywordColorTransformation()
-//        colors = TextFieldDefaults.colors()
-    )
 }
