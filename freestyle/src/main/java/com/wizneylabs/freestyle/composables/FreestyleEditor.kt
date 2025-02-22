d0e45146cee4169cde99988f1dc459c62d02c885
danrae pray <danrae.pray@outlook.com>
2025-02-22 00:34:10 -0600

    added modal navigation drawer to app!

--- freestyle/src/main/java/com/wizneylabs/freestyle/composables/FreestyleEditor.kt
+++ freestyle/src/main/java/com/wizneylabs/freestyle/composables/FreestyleEditor.kt
@@ -3,16 +3,37 @@ package com.wizneylabs.freestyle.composables
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Help
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +51,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.google.rpc.Help
 import com.wizneylabs.freestyle.FreestyleEditorViewModel
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 /********************************************************************
@@ -54,20 +77,23 @@ fun FreestyleEditorApp() {
 
     val navController = rememberNavController();
 
-    NavHost(
-        navController = navController,
-        startDestination = HomeRoute
-    ) {
-        composable<HomeRoute>() {
-            MainMenu(navController, viewModel);
-        }
-        composable<FreestyleEditorRoute>() { backStackEntry ->
+    DetailedDrawerExample(navController) {
 
-            val route: FreestyleEditorRoute = backStackEntry.toRoute();
+        NavHost(
+            navController = navController,
+            startDestination = HomeRoute
+        ) {
+            composable<HomeRoute>() {
+                MainMenu(navController, viewModel);
+            }
+            composable<FreestyleEditorRoute>() { backStackEntry ->
 
-            viewModel.editShader(route.shaderID);
+                val route: FreestyleEditorRoute = backStackEntry.toRoute();
 
-            FreestyleEditor(navController, viewModel);
+                viewModel.editShader(route.shaderID);
+
+                FreestyleEditor(navController, viewModel);
+            }
         }
     }
 }
@@ -91,6 +117,94 @@ fun MainMenu(navController: NavHostController, viewModel: FreestyleEditorViewMod
             }) {
                 Text(id);
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailedDrawerExample(
+    navController: NavHostController,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Column(
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Spacer(Modifier.height(12.dp))
+                    Text("Drawer Title", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleLarge)
+                    HorizontalDivider()
+
+                    Text("Section 1", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleMedium)
+                    NavigationDrawerItem(
+                        label = { Text("Main Menu") },
+                        selected = false,
+                        onClick = { /* Handle click */
+                            navController.navigate(HomeRoute);
+                            scope.launch { drawerState.close() }
+                        }
+                    )
+                    NavigationDrawerItem(
+                        label = { Text("Item 2") },
+                        selected = false,
+                        onClick = { /* Handle click */
+                            scope.launch { drawerState.close() }
+                        }
+                    )
+
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+
+                    Text("Section 2", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleMedium)
+                    NavigationDrawerItem(
+                        label = { Text("Settings") },
+                        selected = false,
+                        icon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
+                        badge = { Text("20") }, // Placeholder
+                        onClick = { /* Handle click */
+                            scope.launch { drawerState.close() }
+                        }
+                    )
+                    NavigationDrawerItem(
+                        label = { Text("Help and feedback") },
+                        selected = false,
+                        icon = { Icon(Icons.AutoMirrored.Outlined.Help, contentDescription = null) },
+                        onClick = { /* Handle click */
+                            scope.launch { drawerState.close() }
+                        },
+                    )
+                    Spacer(Modifier.height(12.dp))
+                }
+            }
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Freestyle Shader Editor") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                if (drawerState.isClosed) {
+                                    drawerState.open()
+                                } else {
+                                    drawerState.close()
+                                }
+                            }
+                        }) {
+                            Icon(Icons.Default.Menu, contentDescription = "Menu")
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            content(innerPadding)
         }
     }
 }
